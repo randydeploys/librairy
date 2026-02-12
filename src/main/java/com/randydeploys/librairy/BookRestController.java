27c10c1af53598feb6d925981998871524f92116
randydeploys <randydeploys@gmail.com>
2026-02-12 23:48:19 +0100

    feat: add Book CRUD (model, repository, controller)

--- src/main/java/com/randydeploys/librairy/BookRestController.java
+++ src/main/java/com/randydeploys/librairy/BookRestController.java
@@ -1,12 +1,50 @@
 package com.randydeploys.librairy;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.randydeploys.librairy.model.Book;
+import com.randydeploys.librairy.repository.BookRepository;
 
-@RestController //
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookRestController {
 
-    //GET POST PUT DELETE (CRUD)
+    private final BookRepository bookRepository;
 
+    public BookRestController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
+    // GET tous les livres
+    @GetMapping
+    public List<Book> getAll() {
+        return bookRepository.findAll();
+    }
 
+    // GET un livre par id
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    // POST créer un livre
+    @PostMapping
+    public Book create(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // PUT modifier un livre
+    @PutMapping("/{id}")
+    public Book update(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
+        return bookRepository.save(book);
+    }
+
+    // DELETE supprimer un livre
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
 }
---
+++ src/main/java/com/randydeploys/librairy/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.randydeploys.librairy.repository;
+
+import com.randydeploys.librairy.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
